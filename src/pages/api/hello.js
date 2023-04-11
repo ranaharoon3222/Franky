f26d39f4ba548581a74d45992e70504528ea203d
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-04-12 00:35:57 +0500

    tes

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
     );
 
     const data = await chimpRequest.json();
-
+    console.log(req.body);
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
