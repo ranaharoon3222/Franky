57693a99ae5e51471f39663c7ba2d6eec45348ed
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-04-12 23:36:56 +0500

    updated route

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import Cors from 'cors';
+import crypto from 'crypto';
+
 const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
 });
@@ -20,21 +22,91 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
   if (req.method !== 'POST') return res.status(200).json({ name: 'Hi Franky' });
+
+  const bodyData = JSON.parse(req?.body);
+  const mg = bodyData.merge_fields;
+
+  const subscriberHash = crypto
+    .createHash('md5')
+    .update(bodyData.email_address.toLowerCase())
+    .digest('hex');
+
+  const url = `https://us11.api.mailchimp.com/3.0/lists/19e84be598/members/${subscriberHash}`;
+  const eventUrl = `https://us11.api.mailchimp.com/3.0/lists/19e84be598/members/${subscriberHash}/events`;
   try {
-    const chimpRequest = await fetch(
-      'https://us11.api.mailchimp.com/3.0/lists/19e84be598/members',
-      {
+    const already = await fetch(url, {
+      headers: {
+        Authorization: process.env.API_KEY,
+      },
+    });
+
+    const isAlready = await already.json();
+    const dupData = isAlready.merge_fields;
+
+    if (isAlready?.status === 404 || isAlready.status === 'archived') {
+      const chimpRequest = await fetch(
+        'https://us11.api.mailchimp.com/3.0/lists/19e84be598/members',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: process.env.API_KEY,
+          },
+          body: req.body,
+        }
+      );
+
+      const eventName = {
+        name: 'dogAdded',
+      };
+
+      await fetch(eventUrl, {
         method: 'POST',
         headers: {
           Authorization: process.env.API_KEY,
         },
-        body: req.body,
-      }
-    );
+        body: JSON.stringify(eventName),
+      });
+      const data = await chimpRequest.json();
+      res.status(200).json(data);
+    } else {
+      const finalData = {
+        DOGNAME: `${dupData.DOGNAME} / ${mg.DOGNAME}`,
+        BREED: `${dupData.BREED} / ${mg.BREED}`,
+        WEIGHT: `${dupData.WEIGHT} / ${mg.WEIGHT}`,
+        DOGAGE: `${dupData.DOGAGE} / ${mg.DOGAGE}`,
+        DOGSEX: `${dupData.DOGSEX} / ${mg.DOGSEX}`,
+        DOGAL: `${dupData.DOGAL} / ${mg.DOGAL}`,
+        DESEXED: `${dupData.DESEXED} / ${mg.DESEXED}`,
+        BODYSCORE: `${dupData.BODYSCORE} / ${mg.BODYSCORE}`,
+        CART: `${dupData.CART} / ${mg.CART}`,
+      };
 
-    const data = await chimpRequest.json();
-    console.log(req.body);
-    res.status(200).json(data);
+      const updatedSubs = {
+        merge_fields: finalData,
+      };
+
+      const chimpRequest = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          Authorization: process.env.API_KEY,
+        },
+        body: JSON.stringify(updatedSubs),
+      });
+      const eventName = {
+        name: 'dogAdded',
+      };
+
+      await fetch(eventUrl, {
+        method: 'POST',
+        headers: {
+          Authorization: process.env.API_KEY,
+        },
+        body: JSON.stringify(eventName),
+      });
+
+      const data = await chimpRequest.json();
+      res.status(200).json(data);
+    }
   } catch (error) {
     console.error(error);
   }
