71469e07b30808cf77ad5ddc6ef23fa336a95742
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2023-04-26 15:09:37 +0200

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -79,6 +79,8 @@ export default async function handler(req, res) {
         DOGAL: `${dupData.DOGAL} / ${mg.DOGAL}`,
         DESEXED: `${dupData.DESEXED} / ${mg.DESEXED}`,
         BODYSCORE: `${dupData.BODYSCORE} / ${mg.BODYSCORE}`,
+        DAILYALL: `${dupData.DAILYALL} / ${mg.DAILYALL}`,
+        MONTHLYALL: `${dupData.MONTHLYALL} / ${mg.MONTHLYALL}`,
         CART: `${dupData.CART} / ${mg.CART}`,
       };
 
