289098c0bca3d9bbfc1aec5ecbeff42773fdc623
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-05-03 14:42:53 +0500

    fix last

--- src/pages/api/file.js
+++ src/pages/api/file.js
@@ -22,20 +22,20 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
   if (req.method !== 'POST') return res.status(200).json({ name: 'Hi Franky' });
-  console.log(req?.body);
 
-  const bodyData = JSON.parse(req?.body);
+  // const bodyData = JSON.parse(req?.body);
 
   const postImageUrl = `https://us11.api.mailchimp.com/3.0/file-manager/files`;
   try {
     const postImg = await fetch(postImageUrl, {
       method: 'POST',
       headers: {
+        'Content-Type': 'application/json',
         Authorization: process.env.API_KEY,
       },
       body: JSON.stringify({
-        name: bodyData.name,
-        file_data: bodyData.imgData,
+        name: req.body.name,
+        file_data: req.body.imgData,
       }),
     });
 
